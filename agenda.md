dbc6db3c012d1c898d91b34090e6a6289cb05c12
gnappo1 <gnappo.1@live.it>
2020-10-20 18:14:12 -0700

    Done.

---
+++ agenda.md
@@ -0,0 +1,17 @@
+# TODAY'S AGENDA
+
+1. Welcome everyone! (3')
+2. A brief intro on how study groups work. (5')
+3. Process Review: How to fork and clone a lab/repository. (3')
+4. Lab Walkthrough Flow: (30')
+    a. Reading error messages.
+    b. Reviewing spec/test files.
+    c. Understanding where to code.
+    d. Figuring out what to code.
+
+5. Methods: general review and rules (5')
+
+6. Quick final exercise: write a method called `price_after_tip` that takes in two integer arguments, `total` and `tip`, and returns the total paid after the tip has been included. (10' <https://repl.it/join/vjkiuile-gnappo1>)
+
+7. Thoughts and feedback. (5')
+8. Have a great night!
--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,15 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
+# Create method `parrot`
+# should output "Squawk!" to the terminal when called without any arguments (FAILED - 1)
+# should return the default phrase, "Squawk!" when called without any arguments (FAILED - 2)
+# should output the given phrase when called with an argument (FAILED - 3)
+# should return the given phrase when called with an argument (FAILED - 4)
+
+def parrot(sentence = "Squawk!") #default argument
+    puts sentence #puts print out the sentence BUT does not return the sentence. Puts returns 'nil'.
+    return sentence #explicit return, aka we included the return keyword. Without it is called 'implicit return'
+end
+
+# When dealing with new methods, I aaalways ask myself these questions and write a note:
+# 1) How does the method work/ what does it do?
+# 2) What the method name is and how many arguments it expects.
+# 3) What does the method return.
